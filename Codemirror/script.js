0eb263774761bc580a16deb58abbf1d8e66445e7
zachdimitrov <zahari_d@abv.bg>
2017-01-24 01:40:54 +0200

    Modify PHP files

--- Codemirror/script.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    var div = document.getElementById('codetest');
-    var myCodeMirror = CodeMirror(div, {
-        value: "function myScript(){return 100;}\n",
-        mode: "javascript"
-    });
-})();
--- MySQL/sqltest.php
+++ MySQL/sqltest.php
@@ -8,15 +8,16 @@
 </head>
 <body>
 
-    <!-- <form method="post">
-    <textarea name="message" id="ta" cols="30" rows="10"></textarea>
+     <form method="post">
+    <textarea name="message" id="ta" cols="69" rows="3"></textarea>
+    <div><input name="user" type="text"></div>
     <div> <input type="submit" value="submit">  </div>
-    </form> -->
+    </form> 
 
 <?php
 
 // ========== VRYZKA S DATABASE ===========
-$connection = mysqli_connect('localhost','Zach', 'pb1186ch', 'testdb');
+$connection = mysqli_connect('localhost','root', 'pb1186ch', 'testdb');
 if(!$connection) {
     echo 'no database';
     exit;
@@ -31,11 +32,13 @@ for ($i=1; $i<500; $i++) {
     $sql = 'INSERT INTO users (user_name, pass, age, is_active) VALUES("test'.$i.'", "qwe'.(123*$i).'", '.rand(3, 65).','.rand(0,1).')';
     mysqli_query($connection, $sql);
 }
-
+*/
 // ========== IZPOLZVANE NA FORMA ZA INPUT NA DANNI ===========
+
 if($_POST) {
     $msg = mysqli_real_escape_string($connection, trim($_POST['message']));
-    $sql='INSERT INTO msg (msg_data) VALUES ("'.$msg.'")';
+    $name = mysqli_real_escape_string($connection, trim($_POST['user']));
+    $sql='INSERT INTO msg (msg_data, user_name) VALUES ("'.$msg.'", "'.$name.'")';
     // echo $sql;
     if (mysqli_query($connection, $sql)) {
         echo 'Data inserted to data base!';
@@ -43,7 +46,32 @@ if($_POST) {
         echo 'Error!';
         echo mysqli_error($connection);
     }
-} */
+}
+
+$qq = mysqli_query($connection, 
+'SELECT user_name, msg_data, msg_id FROM msg ORDER BY msg_id ASC');
+if(!$qq) {
+    echo 'error - no such db exists';
+}
+if($qq->num_rows>0) {
+    echo '<table>
+    <tr>
+    <th>ID</th>
+    <th>MESSAGE</th>
+    <th>USER</>
+    </tr>
+    ';
+    while($rr=$qq->fetch_assoc()) {
+        echo'<tr>
+            <td class="uid">'.$rr['msg_id'].'</td>
+            <td>'.$rr['user_name'].'</td>
+            <td>'.$rr['msg_data'].'</td>
+            </tr>';
+    }
+    echo '</table>';
+} else {
+    echo '<div> No Results </div>';
+}
 
 // ========== DOSTYP DO DATABASE - ZAQVKI ===========
 // update na info
@@ -54,7 +82,7 @@ $q = mysqli_query($connection,
 'SELECT user_id, user_name as un, pass, age, is_active 
 FROM users 
 WHERE age>=18 AND age<=24 AND is_active=0
-ORDER BY user_id 
+ORDER BY age
 ASC LIMIT 0, 500'
 );
 
